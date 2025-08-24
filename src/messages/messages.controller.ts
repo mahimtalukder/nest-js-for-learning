723ab801d8357633aa04a7be49c39b3a9d46f178
Md. Mahim Talukder <mahim4163@gmail.com>
2025-08-24 20:39:15 +0600

    feat: add message handling with DTO validation and HTTP requests

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
+// Controller is class Decorator,  @Get is method Decorator, @Post is method Decorator, @Body is parameter Decorator, @Param is parameter Decorator
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    @Get()
+    listMessages(){
+
+    }
+
+    @Post()
+    createMessage(@Body() body: CreateMessageDto){
+        console.log('Creating message:', body);
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id: string){
+        console.log('Getting message with ID:', id);
+    }
+}

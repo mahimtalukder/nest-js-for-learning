3563e54fad8bfc494c7c8778fae042618e375be4
Md. Mahim Talukder <mahim4163@gmail.com>
2025-08-26 11:17:33 +0600

    feat: initialize NestJS application with basic structure
    - Created package.json with necessary dependencies for NestJS.
    - Added AppController with two routes: /app/asdf and /app/bye.
    - Created AppModule to register the AppController.
    - Implemented main.ts to bootstrap the NestJS application.
    - Configured TypeScript settings in tsconfig.json for NestJS compatibility.

---
+++ scratch/src/main.ts
@@ -0,0 +1,9 @@
+//Module and controller in here
+import { AppModule } from "./app.module";
+import { NestFactory } from "@nestjs/core";
+
+async function bootstrap(){
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
+bootstrap();
